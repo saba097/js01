4ceb4551327f7dae04be4b8cac2865b91571cc7b
saba097 <115619577+saba097@users.noreply.github.com>
2023-11-07 14:33:40 +0400

    last

---
+++ app.js
@@ -0,0 +1,29 @@
+function dog(age)
+{
+    return  age=age*7   // მითითებულ რიცხვს ვამრავლებთ 7 ზე
+     
+}
+console.log(dog(2)) 
+
+
+
+
+
+function dif(x,y) {
+if (x>y)
+{
+   return `${x} მეტია ${y} -ზე`
+}
+    else 
+    {
+        return `${x} ნაკლებია ${y}-ზე`
+    }
+    
+}
+console.log(dif(5,4))
+
+
+
+
+
+
